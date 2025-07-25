db4bd58906b055eb9af7b5ab732740f24be00cc7
a-looft <aidalooft@icloud.com>
2025-07-25 12:11:31 -0400

    use requested colors instead of hardcoded values

--- app/routers/qr_code.py
+++ app/routers/qr_code.py
@@ -47,7 +47,7 @@ async def create_qr_code(request: QRCodeRequest, token: str = Depends(oauth2_sch
         )
 
     # Generate the QR code if it does not exist
-    generate_qr_code(request.url, qr_code_full_path, FILL_COLOR, BACK_COLOR, request.size)
+    generate_qr_code(request.url, qr_code_full_path, request.fill_color, request.back_color, request.size)
     # Return a response indicating successful creation
     return QRCodeResponse(message="QR code created successfully.", qr_code_url=qr_code_download_url, links=links)
 
